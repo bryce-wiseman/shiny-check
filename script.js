84ea0d65f83f7a8733ab9a215f90d351a992db68
bryce-wiseman <119898021+bryce-wiseman@users.noreply.github.com>
2024-02-28 16:17:55 -0500

    added all pokemon and incorporated sprites with pokeAPI

--- script.js
+++ script.js
@@ -1,42 +1,37 @@
-//this is a test
-
-
 const shinyDiv = document.getElementById('shiny-div')
 let shinyImg = document.getElementById('shiny-img')
 let shinyP = document.getElementById('shiny-statement')
 
 const giphyKey = 'ihty4abPppTTdOBSp0Gq8yRXF6GZKrOj'
 const giphyAPI = `https://api.giphy.com/v1/gifs/search?api_key=${giphyKey}&limit=1&rating=g&q=`
+const pokeAPI = 'https://pokeapi.co/api/v2/pokemon/'
 
-const pokeList = './pokemon.txt'
 
-async function pickAPokemon() {
-    let list = await fetch(pokeList)
-    let textList = await list.text()
-    let listToArray = await textList.split('\n')
-    let chosenNum = await Math.floor(Math.random() * listToArray.length)
-    let chosenMon = await listToArray[chosenNum]
-    console.log(chosenMon)
-    return chosenMon
-}
+let randomNum = Math.floor((Math.random() * 1025) + 1)
+console.log(randomNum)
 
-async function makeURL(pokemon) {
-    let formatToAPI = await giphyAPI.concat(pokemon)
-    let searchAPI = await fetch(formatToAPI)
-    let searchedTerm = await searchAPI.json()
-    let endURL = searchedTerm.data[0].images['fixed_height'].url
-    
-    console.log(endURL)
-
-    return endURL
+async function getPokemon(pokeNum) {
+    let getPokemonData = await pokeAPI.concat(pokeNum)
+    let searchAPI = await fetch(getPokemonData)
+    let pokemonData = await searchAPI.json()
+    return pokemonData
 }
 
 async function btnFunc() {
-    let chosenMon = await pickAPokemon()
-    let url = await makeURL(chosenMon)
+    let pokeData = await getPokemon(randomNum)
+    console.log(pokeData)
+
+    let pokemonName = pokeData.name
+    let index = 0;
+    let firstLetter = pokemonName.charAt(index)
+    let capitalize = firstLetter.toUpperCase()
+    let fixedName = pokemonName.replace(firstLetter, capitalize)
+    console.log(fixedName)
+
+    let shinySprite = pokeData.sprites['front_shiny']
+    console.log(shinySprite)
     
     shinyDiv.classList.remove('hidden')
-    shinyImg.src = url
-    shinyP.innerHTML = `Congrats! You caught a shiny ${chosenMon}!`
-    shinyDiv.classList.remove('hidden')
+    shinyImg.src = shinySprite
+    shinyP.innerHTML = `Congrats! You caught a shiny ${fixedName}!`
 }
